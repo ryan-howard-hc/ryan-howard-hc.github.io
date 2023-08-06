3f2e68148c2939fe8fef868ea7b7bdb8cb952fa5
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-08-06 19:12:00 -0400

    week 11

---
+++ app/src/blogweeks/02blogweek11.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './00bloga.css';
+
+const Week11Blog = () => {
+    return (
+      <body className='individual-body'>
+      <div className="individual-blog">
+        
+        <h1>Week 11</h1>
+        <div className="text-container">
+        <h2></h2>
+        <p></p>
+        <p></p>
+        <img src></img>
+        <h2></h2>
+        <p></p>
+      <br></br>
+        <h2></h2>
+        <p></p>
+        <img src></img>
+      </div>
+      </div>
+      </body>
+    );
+  };
+  
+  export default Week11Blog;
