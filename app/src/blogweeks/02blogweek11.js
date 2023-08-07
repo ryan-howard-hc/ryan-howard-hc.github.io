582845d399393432735f439e13832c801e233dd6
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-08-07 13:21:10 -0400

    week 11 cont

--- app/src/blogweeks/02blogweek11.js
+++ app/src/blogweeks/02blogweek11.js
@@ -8,16 +8,11 @@ const Week11Blog = () => {
         
         <h1>Week 11</h1>
         <div className="text-container">
-        <h2></h2>
-        <p></p>
-        <p></p>
-        <img src></img>
-        <h2></h2>
-        <p></p>
-      <br></br>
-        <h2></h2>
-        <p></p>
-        <img src></img>
+        <h2>What would you want to post as a “retrospective”?</h2>
+        <p>JavaScript and Python are like the building blocks of the web. To make them less intimidating, we should have projects that guide us step by step. Let's build things together, play with code, and learn how to fix mistakes. Learning this way makes it all feel less complicated.</p>
+        <p>Onboarding projects are a fantastic idea to facilitate the learning process and help newbies get acclimated to a technology stack or a development environment. Codewar exercises were more beneficial than most of the reading exercises as well. The technical aspects of actually getting started, and knowing what the goal is seemed to be the most anxiety inducing part of the bootcamp.  We need more 'hand-holding', so when we are let go, we know how to get on the ground running. </p>
+        <img src="https://64.media.tumblr.com/3753fd3b76cce4da9e0da270090d7b8a/tumblr_inline_pr8u623UpC1qbycn1_500.png"></img>
+
       </div>
       </div>
       </body>
